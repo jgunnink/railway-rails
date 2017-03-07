3267be409846dad336cbeadc5605683636d0e6c1
JK Gunnink <jgunnink@gmail.com>
2017-03-07 13:28:13 +0800

    finish feature test

--- spec/features/projects/show_project_spec.rb
+++ spec/features/projects/show_project_spec.rb
@@ -6,11 +6,19 @@ feature "User can view project details" do
   let!(:project) { FactoryGirl.create(:project, stage: 1) }
 
   before do
-    visit(client_project_path(project.client))
+    visit(client_project_path(project.client, project))
   end
 
   scenario "user can see what stage the project is up to" do
   	expect(page).to have_content("Project stage: 1")
+  end
+
+  scenario "user can click proceed to move to next stage" do
+  	click_on "Complete stage"
+  	expect(page).to have_flash :notice, "Project was successfully updated."
+
+  	visit(client_project_path(project.client, project))
+  	expect(page).to have_content("Project stage: 2")
   end
 
 end
